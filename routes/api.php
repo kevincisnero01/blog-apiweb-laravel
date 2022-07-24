8ddd13671a27dd6ecb3e7cab3e6101a6366a55e7
Kevin Cisnero <7884578+kevincisnero01@users.noreply.github.com>
2022-07-24 19:11:45 -0400

    Planificacion V2

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\V1\PostController;
+use App\Http\Controllers\API\V1\PostController as Postv1;
+use App\Http\Controllers\API\V2\PostController as Postv2;
 
-Route::apiResource('v1/posts', PostController::class)
+Route::apiResource('v1/posts', Postv1::class)
     ->only(['show','index','destroy']);
+    
+Route::apiResource('v2/posts', Postv2::class)
+->only(['show','index','destroy']);
